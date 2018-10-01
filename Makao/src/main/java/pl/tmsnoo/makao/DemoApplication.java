00318fcb9169e0f62e2784b946104d2042ba0506
Tmsnoo <tomek.laskawiec@gmail.com>
2018-10-01 20:09:03 +0200

    Work in progress

--- Makao/src/main/java/pl/tmsnoo/makao/DemoApplication.java
+++ Makao/src/main/java/pl/tmsnoo/makao/DemoApplication.java
@@ -1,12 +1,24 @@
 package pl.tmsnoo.makao;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import pl.tmsnoo.makao.entity.Card;
+import pl.tmsnoo.makao.repository.CardRepository;
 
 @SpringBootApplication
 public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+	}
+	
+	@Bean
+	CommandLineRunner runner(CardRepository repository) {
+		return args -> {
+			repository.save(new Card());
+		};
 	}
 }
--- Makao/src/main/java/pl/tmsnoo/makao/entity/Card.java
+++ Makao/src/main/java/pl/tmsnoo/makao/entity/Card.java
@@ -1,9 +1,13 @@
 package pl.tmsnoo.makao.entity;
 
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
 
 @Entity
 public class Card {
@@ -14,10 +18,40 @@ public class Card {
 		HEARTS ,
 		SPADES ;
 	}
-	
-	@Enumerated
-	Suit suit ;
-	
+
 	@Id
-	private String id;	
+	@GeneratedValue
+	private Long id;	
+	
+	@Enumerated(EnumType.STRING)
+	@Column(length = 8)
+	private Suit suit ;
+	
+	@NotNull
+	private String figure;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Suit getSuit() {
+		return suit;
+	}
+
+	public void setSuit(Suit suit) {
+		this.suit = suit;
+	}
+
+	public String getFigure() {
+		return figure;
+	}
+
+	public void setFigure(String figure) {
+		this.figure = figure;
+	}
+	
 }
